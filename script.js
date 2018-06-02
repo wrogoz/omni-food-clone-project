b37f770d9c7b09bb000ee8f69d59b7f6ea1e8523
wrogoz <36112414+wrogoz@users.noreply.github.com>
2018-06-02 21:55:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+
+/* display:fixed navigation*/
+$('.js--section-features').waypoint(function(direction){
+    if(direction=='down'){
+        $('nav').addClass('sticky');
+    }
+    else{
+        $('nav').removeClass('sticky');
+    }
+         offset: '60px';
+
+});
+
+
+/* scroll on buttons */
+
+    $('.js--scroll-to-plans').click(function(){
+    $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 900);
+    });
+
+    $('.js--scroll-to-start').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 500);
+        });
+
+
+/* navigation scroll */
+
+
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
+
+
+});
